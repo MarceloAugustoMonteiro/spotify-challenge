74f2c7c91e6e34d13736c0fe141b19b62e95817b
Marcelo Monteiro <marceelomonteeiro@hotmail.com>
2025-10-12 15:15:44 -0300

    refactor: import Sidebar into RootLayout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './globals.css'
+import styles from './layout.module.css'
 import { ReactQueryProvider } from '../providers/react-query'
+import Sidebar from '../components/Sidebar'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body><ReactQueryProvider>{children}</ReactQueryProvider></body>
+      <body>
+        <ReactQueryProvider>
+          <div className={styles.layoutWrapper}>
+            <Sidebar />
+            <main className={styles.mainContent}>
+              {children}
+            </main>
+          </div>
+        </ReactQueryProvider>
+      </body>
     </html>
   )
 }
